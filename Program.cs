c2c0beea6e44f028ebadc03ee17b775c4e639c30
Michael Khanda <michaelkhanda006@gmail.com>
2023-06-28 00:54:01 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Flag_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Console.BackgroundColor = System.ConsoleColor.Black;
+            Console.ForegroundColor = System.ConsoleColor.Black;
+            Console.WriteLine("IIIIIIIIIIIIIIIIIIIIIIIIIIIIIII");
+
+            Console.BackgroundColor = System.ConsoleColor.Black;
+            Console.ForegroundColor = System.ConsoleColor.White;
+            Console.Write("          \\\\       // ");
+
+            Console.BackgroundColor = System.ConsoleColor.Black;
+            Console.ForegroundColor = System.ConsoleColor.Black;
+            Console.WriteLine("IIIIIIIII");
+
+            Console.BackgroundColor = System.ConsoleColor.Black;
+            Console.ForegroundColor = System.ConsoleColor.DarkRed;
+            Console.Write("               ^    ");
+
+
+            Console.BackgroundColor = System.ConsoleColor.Black;
+            Console.ForegroundColor = System.ConsoleColor.Black;
+            Console.WriteLine("IIIIIIIIIII");
+
+            Console.BackgroundColor = System.ConsoleColor.White;
+            Console.ForegroundColor = System.ConsoleColor.White;
+            Console.WriteLine("IIIIIIIIIIIIIIIIIIIIIIIIIIIIIII");
+
+            Console.BackgroundColor = System.ConsoleColor.DarkRed;
+            Console.ForegroundColor = System.ConsoleColor.DarkRed;
+            Console.WriteLine("IIIIIIIIIIIIIIIIIIIIIIIIIIIIIII");
+
+            Console.BackgroundColor = System.ConsoleColor.DarkRed;
+            Console.ForegroundColor = System.ConsoleColor.White;
+            Console.Write("               O      ");
+
+            Console.BackgroundColor = System.ConsoleColor.DarkRed;
+            Console.ForegroundColor = System.ConsoleColor.DarkRed;
+            Console.WriteLine("IIIIIIIII");
+
+            Console.BackgroundColor = System.ConsoleColor.DarkRed;
+            Console.ForegroundColor = System.ConsoleColor.DarkRed;
+            Console.WriteLine("IIIIIIIIIIIIIIIIIIIIIIIIIIIIIII");
+
+            Console.BackgroundColor = System.ConsoleColor.White;
+            Console.ForegroundColor = System.ConsoleColor.White;
+            Console.WriteLine("IIIIIIIIIIIIIIIIIIIIIIIIIIIIIII");
+
+            Console.BackgroundColor = System.ConsoleColor.DarkGreen;
+            Console.ForegroundColor = System.ConsoleColor.DarkGreen;
+            Console.WriteLine("IIIIIIIIIIIIIIIIIIIIIIIIIIIIIII");
+
+            Console.BackgroundColor = System.ConsoleColor.DarkGreen;
+            Console.ForegroundColor = System.ConsoleColor.DarkRed;
+            Console.WriteLine("               v               ");
+
+            Console.BackgroundColor = System.ConsoleColor.DarkGreen;
+            Console.ForegroundColor = System.ConsoleColor.White;
+            Console.Write("          //       \\\\  ");
+
+            Console.BackgroundColor = System.ConsoleColor.DarkGreen;
+            Console.ForegroundColor = System.ConsoleColor.DarkGreen;
+            Console.WriteLine("IIIIIIII");
+
+        }
+    }
+}
